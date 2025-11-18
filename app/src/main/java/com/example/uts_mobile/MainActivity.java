588f09c5da521e73796209928ea02bf6de432d76
aui-tuthousend <mraihan286@gmail.com>
2025-11-18 10:45:28 +0700

    initial commit + intent

---
+++ app/src/main/java/com/example/uts_mobile/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.uts_mobile;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+
+        // Load default fragment
+        if (savedInstanceState == null) {
+            loadFragment(new ExploreFragment());
+        }
+    }
+
+    private boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        if (item.getItemId() == R.id.navigation_explore) {
+            fragment = new ExploreFragment();
+        } else if (item.getItemId() == R.id.navigation_friends) {
+            fragment = new FriendsFragment();
+        } else if (item.getItemId() == R.id.navigation_your_event) {
+            fragment = new YourEventFragment();
+        } else if (item.getItemId() == R.id.navigation_profile) {
+            fragment = new ProfileFragment();
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.fragment_container, fragment);
+            transaction.commit();
+            return true;
+        }
+        return false;
+    }
+}
